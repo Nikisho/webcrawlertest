b0875a1fa0134f686ae04868f215e0b32741c89e
Nikisho <88548303+Nikisho@users.noreply.github.com>
2021-10-15 22:56:42 +0100

    Add files via upload

--- project2b.py
+++ project2b.py
@@ -12,22 +12,23 @@ import requests.exceptions
 #from urllib.parse import urlparse
 #from collections import deque
 
-def linkextractor(URLZ):
+def linkextractor(URLNAME):
     #URL = 'https://www.avantgardevegan.com/'
     
     """nextnew_urls = deque([URL])"""
-    reqs = requests.get(URLZ)
+    reqs = requests.get(URLNAME)
     soup = BeautifulSoup(reqs.text, 'html.parser')
-    
     
     URLS = [] #create empty arraty
     for link in soup.find_all('a'): 
         if 'pinterest' in link.get('href') or 'youtube' in link.get('href') or 'instagram' in link.get('href') or 'contact' in link.get('href') or 'facebook' in link.get('href') or 'twitter' in link.get('href'):
-            CONDITION = False
+            print('Skip!')
+            continue 
         elif 'https' in link.get('href'):
             #print(link.get('href')) #print the href = https://example.link
             data = link.get('href') 
             URLS.append(data) #store in array
+            URLS = list(dict.fromkeys(URLS))
     return URLS    
 #for i in range(10,13):
     #check(URLS[i])
